9d330ddb4014c84094f0b5c64ed750843cf1a3ea
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-08-14 21:05:34 +0700

    WebSocket in Elm

---
+++ src/Chat.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import WebSocket
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+echoServer : String
+echoServer =
+    "wss://echo.websocket.org"
+
+
+type alias Model =
+    { input : String
+    , messages : List String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" [], Cmd.none )
+
+
+type Msg
+    = Input String
+    | Send
+    | NewMessage String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input newInput ->
+            ( { model | input = newInput }, Cmd.none )
+
+        Send ->
+            ( { model | input = "" }, WebSocket.send echoServer model.input )
+
+        NewMessage str ->
+            ( { model | messages = (str :: model.messages) }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    WebSocket.listen echoServer NewMessage
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] (List.map viewMessage model.messages)
+        , input [ onInput Input ] []
+        , button [ type_ "button", onClick Send ] [ text "Send" ]
+        ]
+
+
+viewMessage : String -> Html Msg
+viewMessage msg =
+    div [] [ text msg ]
